78524cf57d290d4c7884b63ec252bea9740a536b
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2022-12-05 23:13:47 +0100

    poprawka zadania 10

--- js/task-10.js
+++ js/task-10.js
@@ -5,12 +5,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 let amount = document.querySelector("input");
-let value = amount.value != "" ? amount.value : 1;
 const initialSize = 30;
 let currentSize = initialSize;
 let newBox;
 function createBoxes() {
-  for ( let i = 1; i <= amount.value; i++) {
+  let value = amount.value != "" ? amount.value : 1;
+  for ( let i = 1; i <= value; i++) {
   newBox = document.createElement("div");
   newBox.style.width = `${currentSize}px`;
   newBox.style.height = `${currentSize}px`;
@@ -21,7 +21,7 @@ function createBoxes() {
   }
 }
 const handleClickDestroy = () => {
-  newBox.remove();
+  boxesElements.innerHTML = "";
   currentSize = initialSize;
 }
 createButton.addEventListener("click", createBoxes);
